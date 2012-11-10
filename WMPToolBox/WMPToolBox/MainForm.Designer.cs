a7ab13ddd4bde6eab43893fc7a8be64d600e4e49
Mike Barber <mike@mbarber.com>
2012-11-10 14:37:48 +0100

    new proj

---
+++ WMPToolBox/WMPToolBox/MainForm.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace WMPToolBox
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Backup = new System.Windows.Forms.Button();
+            this.Restore = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Backup
+            // 
+            this.Backup.Location = new System.Drawing.Point(15, 54);
+            this.Backup.Name = "Backup";
+            this.Backup.Size = new System.Drawing.Size(257, 55);
+            this.Backup.TabIndex = 0;
+            this.Backup.Text = "Backup Library to XML";
+            this.Backup.UseVisualStyleBackColor = true;
+            this.Backup.Click += new System.EventHandler(this.Backup_Click);
+            // 
+            // Restore
+            // 
+            this.Restore.Location = new System.Drawing.Point(15, 150);
+            this.Restore.Name = "Restore";
+            this.Restore.Size = new System.Drawing.Size(257, 55);
+            this.Restore.TabIndex = 1;
+            this.Restore.Text = "Restore Library From XML";
+            this.Restore.UseVisualStyleBackColor = true;
+            this.Restore.Click += new System.EventHandler(this.Restore_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.Restore);
+            this.Controls.Add(this.Backup);
+            this.DoubleBuffered = true;
+            this.Name = "MainForm";
+            this.Text = "WMP Library Tools";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Backup;
+        private System.Windows.Forms.Button Restore;
+    }
+}
+
---
+++ WMPToolBox/WMPToolBox/MainForm.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WMPLib;
+using System.Xml;
+
+namespace WMPToolBox
+{
+    public partial class MainForm : Form
+    {
+
+        private WMP m_WMP;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            m_WMP = new WMP();
+            if (!m_WMP.init())
+                return;
+        }
+
+        private void Backup_Click(object sender, EventArgs e)
+        {
+            // choose file location
+            SaveFileDialog dlg = new SaveFileDialog();
+            dlg.ShowDialog();
+            string filePath = dlg.FileName;
+            // backup library 
+            m_WMP.backup(filePath);
+        }
+
+        private void Restore_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.ShowDialog();
+            string filePath = dlg.FileName;
+            m_WMP.restore(filePath);
+        }
+    }
+
+    public class WMP
+    {
+        private WMPLib.WindowsMediaPlayer m_player;
+        private IWMPMediaCollection m_media;
+        private IWMPStringCollection m_albums;
+        private IWMPStringCollection m_artists;
+
+        public bool init()
+        {
+            m_player = new WMPLib.WindowsMediaPlayer();
+            IWMPSettings2 settings = (IWMPSettings2)m_player.settings;
+            bool b = settings.requestMediaAccessRights("read");
+            if (!b)
+                return b;
+            m_media = m_player.mediaCollection;
+            m_artists = m_media.getAttributeStringCollection("WM/AlbumArtist", "Audio");
+            m_albums = m_media.getAttributeStringCollection("WM/AlbumTitle", "Audio");
+            return true;
+        }
+
+        public void backup(string path)
+        {
+            XmlTextWriter writer = new XmlTextWriter(path, null);
+            writer.Formatting = Formatting.Indented;
+
+            writer.WriteStartDocument();
+
+            writer.WriteStartElement("library");
+
+            IWMPPlaylist allList =  m_media.getAll();
+            int count = allList.count;
+            for (int i = 0; i < count; ++i)
+            {
+                IWMPMedia mediaItem = allList.get_Item(i);
+                string url = mediaItem.sourceURL;
+                writer.WriteStartElement("MediaItem");
+                writer.WriteAttributeString("url", url);
+
+                int attCount = mediaItem.attributeCount;
+                for (int j = 0; j < attCount; ++j)
+                {
+                    string attrName = mediaItem.getAttributeName(j);
+                    writer.WriteStartElement(attrName);
+                    string attrValue = mediaItem.getItemInfo(attrName);
+                    writer.WriteAttributeString("value", attrValue);
+                    writer.WriteEndElement();
+                }
+
+                writer.WriteEndElement();
+            }
+            writer.WriteEndDocument();
+            writer.Close();
+        }
+
+        public void restore(string path)
+        {
+            XmlTextReader reader = new XmlTextReader(path);
+            while (reader.Read())
+            {
+                if (reader.NodeType == XmlNodeType.Element)
+                {
+
+                }
+            }
+        }
+
+
+    }
+}
